9ce26edf8f5270d9e9996127c27b0ffa67add8e1
zinirun <heojj97@naver.com>
2020-05-29 18:20:25 +0900

    Create Rep4_DP_Cost.java

---
+++ src/algo/Rep4_DP_Cost.java
@@ -0,0 +1,22 @@
+package algo;
+
+import java.util.Scanner;
+
+public class Rep4_DP_Cost {
+	static void calcString(String s1, String s2) {
+	}
+	public static void main(String[] args) {
+		int cost_I = 1, cost_D = 1, cost_C = 2;
+		String x = null, y = null;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("String A: ");
+		x = sc.next();
+		System.out.print("String B: ");
+		y = sc.next();
+		sc.close();
+		
+		
+		
+	}
+}
